b6412e4e0b9fcab0ae793b52eaf96be5c5a91320
shaopx <shaopx@gmail.com>
2016-11-07 23:30:07 +0800

    增加了数据上传代码

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,14 +12,6 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-        final List<MongoCredential> credentialsList = new ArrayList<MongoCredential>();
-        credentialsList.add(MongoCredential.createCredential("shaopx", "mydb", "shaopx2016".toCharArray()));
-        MongoClient mongoClient = new MongoClient(new ServerAddress("52.27.4.79", 27017), credentialsList);
-        MongoDatabase db = mongoClient.getDatabase("mydb");
-        ListCollectionsIterable<Document> documents = db.listCollections();
-        System.out.println("documents:" + documents.toString());
-        db.createCollection("testcollections");
-        MongoCollection<Document> testcollections = db.getCollection("testcollections");
 
     }
 }
