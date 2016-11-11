86440be1141ada046be3ec1540e71083855b4bda
shaopengxiang <shaopx@gmail.com>
2016-11-11 17:12:11 +0800

    增加了网络爬虫代码

---
+++ src/main/groovy/CrawlerTest.groovy
@@ -0,0 +1,20 @@
+import edu.uci.ics.crawler4j.crawler.Page
+import edu.uci.ics.crawler4j.crawler.WebCrawler
+import edu.uci.ics.crawler4j.parser.HtmlParseData
+import edu.uci.ics.crawler4j.url.WebURL
+
+import javax.swing.JTabbedPane
+import java.util.regex.Pattern
+
+/**
+ * Created by shaopengxiang on 2016/11/11.
+ */
+class CrawlerTest extends WebCrawler {
+
+
+
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
