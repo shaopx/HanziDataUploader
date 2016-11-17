c6f6b8e30562220ed2bea04eb81d2f1fa38f8cbf
shaopx <shaopx@gmail.com>
2016-11-18 00:28:03 +0800

    增加了几个数据抓取解析的文件

--- src/main/java/ByteTest.java
+++ src/main/java/ByteTest.java
@@ -1,3 +1,10 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.*;
+import java.nio.channels.FileChannel;
+import java.sql.*;
+import java.io.*;
+import java.sql.DriverManager;
 import java.util.Arrays;
 
 /**
@@ -9,8 +16,7 @@ public class ByteTest {
     /**
      * int整数转换为4字节的byte数组
      *
-     * @param i
-     *            整数
+     * @param i 整数
      * @return byte数组
      */
     public static byte[] intToByte4(int i) {
@@ -25,8 +31,7 @@ public class ByteTest {
     /**
      * long整数转换为8字节的byte数组
      *
-     * @param lo
-     *            long整数
+     * @param lo long整数
      * @return byte数组
      */
     public static byte[] longToByte8(long lo) {
@@ -41,8 +46,7 @@ public class ByteTest {
     /**
      * short整数转换为2字节的byte数组
      *
-     * @param s
-     *            short整数
+     * @param s short整数
      * @return byte数组
      */
     public static byte[] unsignedShortToByte2(int s) {
@@ -55,8 +59,7 @@ public class ByteTest {
     /**
      * byte数组转换为无符号short整数
      *
-     * @param bytes
-     *            byte数组
+     * @param bytes byte数组
      * @return short整数
      */
     public static int byte2ToUnsignedShort(byte[] bytes) {
@@ -66,10 +69,8 @@ public class ByteTest {
     /**
      * byte数组转换为无符号short整数
      *
-     * @param bytes
-     *            byte数组
-     * @param off
-     *            开始位置
+     * @param bytes byte数组
+     * @param off   开始位置
      * @return short整数
      */
     public static int byte2ToUnsignedShort(byte[] bytes, int off) {
@@ -81,10 +82,8 @@ public class ByteTest {
     /**
      * byte数组转换为int整数
      *
-     * @param bytes
-     *            byte数组
-     * @param off
-     *            开始位置
+     * @param bytes byte数组
+     * @param off   开始位置
      * @return int整数
      */
     public static int byte4ToInt(byte[] bytes, int off) {
@@ -95,22 +94,207 @@ public class ByteTest {
         return (b0 << 24) | (b1 << 16) | (b2 << 8) | b3;
     }
 
-    public static void main(String[] args){
+
+    public void sqliteTest() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:poem_shangxi.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+
+            ResultSet rs = statement.executeQuery("select _id, shangxi from shangxi");
+
+//            while(rs.)
+
+            while (rs.next()) {
+                System.out.println(rs.getString("word") + rs.getString("p1") + rs.getString("p2"));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+
+    public byte[] getContent(String filePath) throws IOException {
+        File file = new File(filePath);
+        long fileSize = file.length();
+        if (fileSize > Integer.MAX_VALUE) {
+            System.out.println("file too big...");
+            return null;
+        }
+        FileInputStream fi = new FileInputStream(file);
+        byte[] buffer = new byte[(int) fileSize];
+        int offset = 0;
+        int numRead = 0;
+        while (offset < buffer.length
+                && (numRead = fi.read(buffer, offset, buffer.length - offset)) >= 0) {
+            offset += numRead;
+        }
+        // 确保所有数据均被读取
+        if (offset != buffer.length) {
+            throw new IOException("Could not completely read file "
+                    + file.getName());
+        }
+        fi.close();
+        return buffer;
+    }
+
+    /**
+     * the traditional io way
+     *
+     * @param filename
+     * @return
+     * @throws IOException
+     */
+    public static byte[] toByteArray(String filename) throws IOException {
+
+        File f = new File(filename);
+        if (!f.exists()) {
+            throw new FileNotFoundException(filename);
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream((int) f.length());
+        BufferedInputStream in = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(f));
+            int buf_size = 1024;
+            byte[] buffer = new byte[buf_size];
+            int len = 0;
+            while (-1 != (len = in.read(buffer, 0, buf_size))) {
+                bos.write(buffer, 0, len);
+            }
+            return bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bos.close();
+        }
+    }
+
+    /**
+     * NIO way
+     *
+     * @param filename
+     * @return
+     * @throws IOException
+     */
+    public static byte[] toByteArray2(String filename) throws IOException {
+
+        File f = new File(filename);
+        if (!f.exists()) {
+            throw new FileNotFoundException(filename);
+        }
+
+        FileChannel channel = null;
+        FileInputStream fs = null;
+        try {
+            fs = new FileInputStream(f);
+            channel = fs.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate((int) channel.size());
+            while ((channel.read(byteBuffer)) > 0) {
+                // do nothing
+                // System.out.println("reading");
+            }
+            return byteBuffer.array();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fs.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Mapped File way MappedByteBuffer 可以在处理大文件时，提升性能
+     *
+     * @param filename
+     * @return
+     * @throws IOException
+     */
+    public static byte[] toByteArray3(String filename) throws IOException {
+
+        FileChannel fc = null;
+        try {
+            fc = new RandomAccessFile(filename, "r").getChannel();
+            MappedByteBuffer byteBuffer = fc.map(FileChannel.MapMode.READ_ONLY, 0,
+                    fc.size()).load();
+            System.out.println(byteBuffer.isLoaded());
+            byte[] result = new byte[(int) fc.size()];
+            if (byteBuffer.remaining() > 0) {
+                // System.out.println("remain");
+                byteBuffer.get(result, 0, byteBuffer.remaining());
+            }
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                fc.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
 
         //31 33 66 34 33 39 37 32 36 64 32 64 34 35 32 32
         //31 33 66 34 33 39 37 32 36 64 32 64 34 35 32 32
-        String str ="13f439726d2d4522";
+        String str = "13f439726d2d4522";
         byte[] key = str.getBytes();
-        System.out.println("key key key temps:"+ Arrays.toString(key));
+        System.out.println("key key key temps:" + Arrays.toString(key));
         System.out.println("----");
-        for(int i=0;i<key.length;i++){
+        for (int i = 0; i < key.length; i++) {
             String hex = Integer.toHexString(key[i] & 0xFF);
-            if (hex.length() == 1)
-            {
+            if (hex.length() == 1) {
                 hex = '0' + hex;
             }
             System.out.print(hex.toUpperCase() + " ");
         }
         System.out.println();
+
+        byte[] arrayOfByte = toByteArray("D:/data/kkpoem/dict/xxx.data");
+        if (arrayOfByte != null) {
+            int i = 16 * (arrayOfByte.length / 16);
+            if (i >= 16) {
+                String result = doSomething(arrayOfByte, i);
+                System.out.print(result + " ");
+            }
+        }
+    }
+
+    public static byte[] b(byte[] paramArrayOfByte, int paramInt) {
+        try {
+            Cipher localCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            localCipher.init(2, new SecretKeySpec("13f439726d2d4522".getBytes(), "AES"));
+            byte[] arrayOfByte = localCipher.doFinal(paramArrayOfByte, 0, paramInt);
+            return arrayOfByte;
+        } catch (Exception localException) {
+            localException.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String doSomething(byte[] arrayOfByte, int i) {
+        return new String(b(arrayOfByte, i));
     }
 }
